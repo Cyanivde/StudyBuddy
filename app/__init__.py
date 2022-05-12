caa4cc297ac6d5b34705d7b4a2a35c59112b5961
cyanivde <cyanivde@gmail.com>
2022-05-12 22:50:54 +0300

    Chapter 5

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,14 @@
-from flask import Flask
 from config import Config
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask
+from flask_login import LoginManager
 from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+
+from app import models, routes
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
-
-from app import routes, models
+login = LoginManager(app)
+login.login_view = 'login'
--- app/models.py
+++ app/models.py
@@ -1,15 +1,27 @@
 from datetime import datetime
-from app import db
 
-class User(db.Model):
+from flask_login import UserMixin
+from werkzeug.security import check_password_hash, generate_password_hash
+
+from app import db, login
+
+
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
     posts = db.relationship('Post', backref='author', lazy='dynamic')
 
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
     def __repr__(self):
         return '<User {}>'.format(self.username)
+
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -19,3 +31,8 @@ class Post(db.Model):
 
     def __repr__(self):
         return '<Post {}>'.format(self.body)
+
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
