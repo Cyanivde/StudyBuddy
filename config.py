7140d5dc706ea8ae64bc227eec6ec47a2babfac8
cyanivde <cyanivde@gmail.com>
2022-05-12 20:46:05 +0300

    Chapter 4

--- config.py
+++ config.py
@@ -6,3 +6,5 @@ load_dotenv(os.path.join(basedir, '.env'))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
