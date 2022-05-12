71764a78a0ec43fe04921249491c74ebd598d480
cyanivde <cyanivde@gmail.com>
2022-05-12 23:22:06 +0300

    Fixes

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,8 @@
-from config import Config
 from flask import Flask
-from flask_login import LoginManager
-from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
-
-from app import models, routes
+from flask_migrate import Migrate
+from flask_login import LoginManager
+from config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -12,3 +10,5 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
+
+from app import routes, models
--- app/routes.py
+++ app/routes.py
@@ -11,18 +11,7 @@ from app.models import User
 @app.route('/index')
 @login_required
 def index():
-    user = {'username': 'Miguel'}
-    posts = [
-        {
-            'author': {'username': 'John'},
-            'body': 'Beautiful day in Portland!'
-        },
-        {
-            'author': {'username': 'Susan'},
-            'body': 'The Avengers movie was so cool!'
-        }
-    ]
-    return render_template("index.html", title='Home Page', posts=posts)
+    return render_template("index.html", title='Home Page')
 
 
 @app.route('/login', methods=['GET', 'POST'])
