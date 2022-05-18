8419f8755aae4ad78c2573948c8672e05001a7f2
cyanivde <cyanivde@gmail.com>
2022-05-18 19:50:31 +0300

    Improve user system

--- app/forms.py
+++ app/forms.py
@@ -1,23 +1,33 @@
+from curses import ERR
 from flask_wtf import FlaskForm
 from wtforms import BooleanField, PasswordField, StringField, SubmitField
-from wtforms.validators import DataRequired, Email, EqualTo, ValidationError
+from wtforms.validators import DataRequired, Email, EqualTo, ValidationError, Regexp, Length
 
 from app.models import User
 
+ERR_USERNAME_EMPTY = "Username must not be empty."
+ERR_PASSWORD_EMPTY = "Password must not be empty."
+ERR_EMAIL_EMPTY = "Email must not be empty."
+
 
 class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
+    username = StringField('Username', validators=[
+                           DataRequired(message=ERR_USERNAME_EMPTY)])
+    password = PasswordField('Password', validators=[
+        DataRequired(message=ERR_PASSWORD_EMPTY)])
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
 
 
 class RegistrationForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
-    email = StringField('Email', validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
+    username = StringField('Username', validators=[
+        DataRequired(message=ERR_USERNAME_EMPTY), Regexp('^\w{5,20}$', message="Username must contain 5-20 letters, numbers or underscore.")])
+    email = StringField('Email', validators=[
+        DataRequired(message=ERR_EMAIL_EMPTY), Email()])
+    password = PasswordField('Password', validators=[
+        DataRequired(message=ERR_PASSWORD_EMPTY), Regexp('^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$', message="Password must contain 8+ characters, of which at least one letter and one number.")])
     password2 = PasswordField(
-        'Repeat Password', validators=[DataRequired(), EqualTo('password')])
+        'Repeat Password', validators=[DataRequired(ERR_PASSWORD_EMPTY), EqualTo('password')])
     submit = SubmitField('Register')
 
     def validate_username(self, username):
--- app/routes.py
+++ app/routes.py
@@ -21,7 +21,8 @@ def login():
         return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        user = User.query.filter_by(username=form.username.data).first()
+        user = User.query.filter_by(
+            username=form.username.data.lower()).first()
         if user is None or not user.check_password(form.password.data):
             flash('Invalid username or password')
             return redirect(url_for('login'))
@@ -45,9 +46,11 @@ def welcome():
         return redirect(url_for('index'))
     form = RegistrationForm()
     if form.validate_on_submit():
-        user = User(username=form.username.data, email=form.email.data)
+        user = User(username=form.username.data.lower(),
+                    email=form.email.data.lower())
         user.set_password(form.password.data)
         db.session.add(user)
         db.session.commit()
-        return redirect(url_for('login'))
+        login_user(user)
+        return redirect(url_for('index'))
     return render_template('welcome.html', title='welcome', form=form)
