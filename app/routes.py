722f80f672c10693dfc31f3eb8a9741f8b68d326
cyanivde <cyanivde@gmail.com>
2022-08-12 21:46:33 +0300

    Formatting updates

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,5 @@
 from flask import flash, redirect, render_template, request, url_for
-from flask_login import current_user, login_required, login_user, logout_user
-from sqlalchemy import all_
+from flask_login import current_user, login_user, logout_user
 from werkzeug.urls import url_parse
 
 from app import app, db
