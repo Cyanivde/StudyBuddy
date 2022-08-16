57c37f42125dee76b085d42d66558ff3db35b4dd
cyanivde <cyanivde@gmail.com>
2022-08-16 21:04:49 +0300

    exactly same db

--- app/models.py
+++ app/models.py
@@ -11,12 +11,20 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
 
 class Subject(db.Model):
@@ -49,7 +57,7 @@ class ResourceToCourse(db.Model):
 
 class ResourceToUser(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     resource_id = db.Column(db.Integer)
     done = db.Column(db.Integer)
 
