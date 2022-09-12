4f7d9a02a2f067fe4bb7d08496bd231f19235408
cyanivde <cyanivde@gmail.com>
2022-09-12 21:31:58 +0300

    remove search

--- app/course.py
+++ app/course.py
@@ -22,11 +22,11 @@ def _course(course_id, tab):
             resources_df = _add_fake_rows(resources_df)
             all_subjects = _get_subjects(resources_df)
 
-            resources_df = _filter_resources(resources_df, query=request.form.get('query'), subject=request.form.getlist('subject'))
+            resources_df = _filter_resources(resources_df, subject=request.form.getlist('subject'))
 
     multi_resources = dict()
     if len(resources_df) > 0:
         for header in resources_df['header']:
             multi_resources[header] = resources_df[resources_df['header'] == header]
 
-    return render_template('course.html', subjects=all_subjects, filtered_subjects=request.form.getlist('subject'), course=course, current_search=request.form.get('query'), resources=multi_resources, tab=tab)
+    return render_template('course.html', subjects=all_subjects, filtered_subjects=request.form.getlist('subject'), course=course, resources=multi_resources, tab=tab)
