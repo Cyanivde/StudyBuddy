656b61339939eaf01d9979ad579f78e0ab641b0c
cyanivde <cyanivde@gmail.com>
2022-09-28 14:54:16 +0300

    minor improvements

--- app/course.py
+++ app/course.py
@@ -2,6 +2,7 @@ from flask import render_template, request
 from flask_login import current_user
 from app.models import Course
 from app.utils import _get_subjects, _fetch_resources
+import pandas as pd
 
 
 def _course(course_id, tab):
@@ -9,7 +10,7 @@ def _course(course_id, tab):
 
     resources_df = _fetch_resources(course_id, tab)
     if len(resources_df) == 0:
-        return render_template('course.html', subjects=[], filtered_subject=[], course=course, resources=dict())
+        return render_template('course.html', subjects=[], filtered_subject=[], course=course, resources=pd.DataFrame())
 
     # resources_df = _add_fake_rows(resources_df)
     all_subjects = _get_subjects(resources_df)
@@ -20,9 +21,5 @@ def _course(course_id, tab):
             # resources_df = _add_fake_rows(resources_df)
             all_subjects = _get_subjects(resources_df)
 
-    multi_resources = dict()
-    if len(resources_df) > 0:
-        for header in resources_df['header']:
-            multi_resources[header] = resources_df[resources_df['header'] == header]
-
-    return render_template('course.html', subjects=all_subjects, filtered_subjects=request.form.getlist('subject'), course=course, resources=multi_resources, tab=tab)
+    print(resources_df)
+    return render_template('course.html', subjects=all_subjects, filtered_subjects=request.form.getlist('subject'), course=course, resources=resources_df, tab=tab)
