6a24152d1be41a19c5cc2336c22827869a3175ce
cyanivde <cyanivde@gmail.com>
2022-10-14 17:15:16 +0300

    minor improvement

--- app/resource.py
+++ app/resource.py
@@ -1,9 +1,10 @@
 from flask import render_template
-from app.utils import _fetch_resource_df
+from app.utils import _fetch_resource_df, _fetch_course
 
 
 def _resource(institute, institute_course_id, resource_id):
     resource_df = _fetch_resource_df(resource_id)
+
     resource = resource_df.iloc[0]
-    print(resource)
-    return render_template('resource.html', resource=resource)
+    course = _fetch_course(int(resource.course_id))
+    return render_template('resource.html', resource=resource, course=course)
