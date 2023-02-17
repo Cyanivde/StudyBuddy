6c8168a9961ca060abe265e247ce83ef9f3cddd2
Yaniv Carmel <t-ycarmel@microsoft.com>
2023-02-17 14:42:40 +0200

    minor changes for documentation

--- .flaskenv
+++ .flaskenv
@@ -1,2 +1,1 @@
 FLASK_APP=application.py
-FLASK_ENV=development
--- config.py
+++ config.py
@@ -8,7 +8,7 @@ load_dotenv(os.path.join(basedir, '.env'))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'demo_database.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
     MAIL_PORT = os.environ.get('MAIL_PORT')
