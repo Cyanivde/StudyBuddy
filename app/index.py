12ab1b089a381d479a75fa86d7ef3c2d5b730a85
cyanivde <cyanivde@gmail.com>
2023-02-19 19:44:40 +0200

    landing page redesign

--- app/index.py
+++ app/index.py
@@ -4,4 +4,4 @@ from app.utils import _fetch_courses
 
 
 def _index():
-    return render_template('index2.html', courses=_fetch_courses())
+    return render_template('index.html', courses=_fetch_courses())
